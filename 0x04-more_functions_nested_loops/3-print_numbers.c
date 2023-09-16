2596e6775f5dc6f579891b259467ec659a6c769d
samson3reeps <samson3reeps@gmail.com>
2023-09-15 20:01:54 -0700

    fix

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,19 @@
-#include "main.h"
+#include <unistd.h> // Include the header for write function
+
+/**
+ * _putchar - Writes a character to the standard output (stdout)
+ * @c: The character to be written
+ *
+ * Return: On success, 1 is returned. On error, -1 is returned.
+ */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
 
 /**
  * print_numbers - Prints the numbers 0-9
  */
-
 void print_numbers(void)
 {
     char c;
@@ -13,4 +23,10 @@ void print_numbers(void)
         _putchar(c);
     }
     _putchar('\n');
+}
+
+int main(void)
+{
+    print_numbers();
+    return (0);
 }
