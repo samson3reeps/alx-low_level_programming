69d7b4eb6544ac8de7c2e454e2367179fd52f63a
samson3reeps <samson3reeps@gmail.com>
2023-10-05 20:46:15 -0700

    malloc_free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * create_array - create array of size size and assign char c
@@ -7,20 +8,25 @@
  * @c: char to assign
  * Description: create array of size size and assign char c
  * Return: pointer to array, NULL if fail
- *
  */
 
 char *create_array(unsigned int size, char c)
-{
-	char *str;
-	unsigned int i;
 
-	str = malloc(sizeof(char) * size);
-	if (size == 0 || str == NULL)
+{
+
+	unsigned int i;
+	char *arr;
+
+	if (size == 0)
+		return (NULL);
+	arr = malloc(sizeof(char) * size);
+
+	if (arr == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		str[i] = c;
-	return (str);
+		arr[i] = c;
+	return (arr);
+
 }
 
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
 
 char *create_array(unsigned int size, char c);
 int _putchar(char c);
@@ -7,7 +9,6 @@ char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av)
 char **strtow(char *str);
 
 #endif
